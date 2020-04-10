4b81137ae927ddb6bc0cf902b49de90e901ef989
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-10 19:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email){
+  let formData {
+    name: name,
+    email: email
+  }
+
+  let configObj {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+    body: JSON:stringify(formData)
+  }
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(object){
+      console.log(object)
+    })
+    .catch(function(error){
+      console.log(error.message);
+    })
+}
